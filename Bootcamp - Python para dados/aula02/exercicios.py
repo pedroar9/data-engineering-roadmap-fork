6af69e9510cb854e0959859e002fdeb4ede4c016
Pedro Assis <pedrocarlos.assis@gmail.com>
2025-03-24 22:03:24 -0300

    :sparkles: feat: Atualizando exercícios de 1 a 5

--- Bootcamp - Python para dados/aula02/exercicios.py
+++ Bootcamp - Python para dados/aula02/exercicios.py
@@ -1,31 +1,51 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+print("Exercício 1 - De soma de dois números inteiros")
 
 numero_1 = int(input("Digite um número inteiro: "))
 numero_2 = int(input("Digite outro número inteiro: "))
-
 resultado = numero_1 + numero_2
-print(f"A soma de {numero_1} + {numero_2} é {resultado}")
 
+print(f"A soma de {numero_1} + {numero_2} é: {resultado}")
+print("\n")
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+print("Exercício 2 - Resto da divisão por 5")
 
 numero_1 = int(input("Digite um número inteiro: "))
-
 resultado = numero_1 // 5
-print(f"O resto da divisão de {numero_1} por 5 é {resultado}")
+
+print(f"O resto da divisão de {numero_1} por 5 é: {resultado}")
+print("\n")
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+print("Exercício 3 - Multiplicação de dois números inteiros")
 
 numero_1 = int(input("Digite um número inteiro: "))
 numero_2 = int(input("Digite outro número inteiro: "))
+resultado = numero_1 * numero_2
 
+print(f"A multiplicação de {numero_1} * {numero_2} é: {resultado}")
+print("\n")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+print("Exercício 4 - Divisão inteira de dois números inteiros")
+
+numero_1 = int(input("Digite um número inteiro: "))
+numero_2 = int(input("Digite outro número inteiro: "))
 resultado = numero_1 // numero_2
-print(f"A divisão inteira de {numero_1} por {numero_2} é {resultado}")
+
+print(f"A divisão inteira de {numero_1} por {numero_2} é: {resultado}")
+print("\n")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+print("Exercício 5 - Quadrado de um número inteiro")
+numero_1 = int(input("Digite um número inteiro: "))
+resultado = numero_1 ** 2   
+print(f"O quadrado de {numero_1} é: {resultado}")   
+print("\n")
 
 # #### Números de Ponto Flutuante (`float`)
 
