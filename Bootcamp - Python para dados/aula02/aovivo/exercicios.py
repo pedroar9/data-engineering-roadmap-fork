eb36ab1efcf431a4cba5ddf791b9cd54e7f6e960
Pedro Assis <pedrocarlos.assis@gmail.com>
2025-03-24 21:55:24 -0300

    :sparkles: feat: Atualizando exercício 4

--- Bootcamp - Python para dados/aula02/aovivo/exercicios.py
+++ Bootcamp - Python para dados/aula02/aovivo/exercicios.py
@@ -5,6 +5,7 @@ import math
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 numero_01 = int(input("Inserir um numero inteiro: "))
--- Bootcamp - Python para dados/aula02/exercicios.py
+++ Bootcamp - Python para dados/aula02/exercicios.py
@@ -1,9 +1,30 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_1 = int(input("Digite um número inteiro: "))
+numero_2 = int(input("Digite outro número inteiro: "))
+
+resultado = numero_1 + numero_2
+print(f"A soma de {numero_1} + {numero_2} é {resultado}")
+
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero_1 = int(input("Digite um número inteiro: "))
+
+resultado = numero_1 // 5
+print(f"O resto da divisão de {numero_1} por 5 é {resultado}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero_1 = int(input("Digite um número inteiro: "))
+numero_2 = int(input("Digite outro número inteiro: "))
+
+resultado = numero_1 // numero_2
+print(f"A divisão inteira de {numero_1} por {numero_2} é {resultado}")
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
