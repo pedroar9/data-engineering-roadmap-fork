06c94e4bf872b8485e8136598681e374febe06f6
Pedro Assis <pedrocarlos.assis@gmail.com>
2025-03-24 21:31:45 -0300

    :sparkles: feat: Atualizando desafio_01

---
+++ Bootcamp - Python para dados/aula01/aovivo/exemplo_02.py
@@ -0,0 +1,26 @@
+
+# Tipo de dados primitivos
+
+numero = 10 # INT
+numero_decimal = 4.80 # FLOAT
+texto = "Olá, mundo!" # STRING
+verdadeiro = True # BOOLEAN
+falso = False # BOOLEAN 
+
+
+print(type(texto)) # INT
+
+# Uma varíavel sempre minuscula
+# Se for composta, separar com underline
+# Ex: nome_completo ou nomeCompleto ou NomeCompleto ou nomecompleto
+
+# Uma váriavel com _ no inicio é privada/secreta
+# Ex: _nome ou _nome_completo
+
+# Não pode usar palavras reservadas
+# Ex: class, def, if, else, elif, while, for, in, not, and, or, return, break, continue, pass, lambda, 
+# try, except, finally, raise, assert, import, from, as, with, is, None, True, False, global, nonlocal, del, yield 
+
+# Uma constante sempre maiuscula
+# Se for composta, separar com underline
+# Ex: SENHA_AWS ou SENHAAWS ou SENHA_AWS ou senha_aws    
---
+++ Bootcamp - Python para dados/aula01/aovivo/exemplo_03.py
@@ -0,0 +1,9 @@
+# Exercício 03: Refatore o exercício 02 atribuindo variáveis
+
+# Exemplo 02
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+# print(len(input("Digite o seu nome: ")))
+
+nome = input("Digite o seu nome: ")
+tamanho_nome = len(nome)
+print(tamanho_nome)
